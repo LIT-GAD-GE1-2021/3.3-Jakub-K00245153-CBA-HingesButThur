54f4c842b0bf577f10bf342269cdeaeec0c5ade0
gad2jaykzin <cz21282074@gmail.com>
2021-01-28 11:00:12 +0000

    h
    h

---
+++ Assets/Scripts/AutoSwitchController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AutoSwitchController : MonoBehaviour
+{
+    private bool switchOff = true;
+    private Animator switchAnimator;
+    void Awake()
+    {
+        switchAnimator = gameObject.GetComponent<Animator>();
+        turnOff();
+    }
+
+    public void turnOn()
+    {
+        switchOff = false;
+        switchAnimator.SetBool("SwitchOff", switchOff);
+    }
+
+    public void turnOff()
+    {
+        switchOff = true;
+        switchAnimator.SetBool("SwitchOff", switchOff);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Someone entered the switch trigger");
+
+        this.turnOn();
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Someone left the switch trigger");
+
+        this.turnOff();
+    }
+}
+
---
+++ Assets/Scripts/ManualSwitchController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ManualSwitchController : MonoBehaviour
+{ 
+    private bool switchOff = true;
+    private bool switchEnabled = false;
+
+    private Animator switchAnimator;
+
+    void Awake()
+    {
+        switchAnimator = gameObject.GetComponent<Animator>();
+        turnOff();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            toggleSwitch();
+        }
+    }
+
+    public void turnOn()
+    {
+        switchOff = false;
+        switchAnimator.SetBool("SwitchOff", switchOff);
+    }
+
+    public void turnOff()
+    {
+        switchOff = true;
+        switchAnimator.SetBool("SwitchOff", switchOff);
+    }
+
+    public void enableSwitch()
+    {
+        switchEnabled = true;
+    }
+
+    public void disableSwitch()
+    {
+        switchEnabled = false;
+    }
+
+    public void toggleSwitch()
+    {
+        if (switchEnabled == true)
+        {
+            switchOff = !switchOff;
+            switchAnimator.SetBool("SwitchOff", switchOff);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        this.enableSwitch();
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        this.disableSwitch();
+    }
+
+}
